c7964951cb52d38a32a69dbef8a9987c14fba179
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-25 14:50:35 -0300

    feat: func adicionar para a Pagina Carrinho

--- src/app/(loja)/carrinho/page.tsx
+++ src/app/(loja)/carrinho/page.tsx
@@ -1,12 +1,24 @@
 'use client'
+import AreaItemCarrinho from "@/components/carrinho/AreaItemCarrinho";
 import Pagina from "@/components/template/Pagina";
+import useCarrinho from "@/data/hooks/useCarrinho";
 
 
 export default function PaginaCarrinho() {
 
+    const { itens, adicionar } = useCarrinho()
+
     return (
         <Pagina>
-            Carrinho
+            <div className="flex flex-col gap-5">
+                {itens.map(item =>
+                    <AreaItemCarrinho
+                        key={item.produto.id}
+                        item={item}
+                        adicionar={(item) => adicionar(item.produto)}
+                    />
+                )}
+            </div>
         </Pagina>
     )
 }
