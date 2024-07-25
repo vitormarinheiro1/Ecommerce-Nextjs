9d2af566effe3df76f210794eae16aa1bbbb0fe1
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-25 15:14:52 -0300

    feat: TotalCarrinho

--- src/app/(loja)/carrinho/page.tsx
+++ src/app/(loja)/carrinho/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import AreaItemCarrinho from "@/components/carrinho/AreaItemCarrinho";
 import CarrinhoVazio from "@/components/carrinho/CarrinhoVazio";
+import TotalCarrinho from "@/components/carrinho/TotalCarrinho";
 import Pagina from "@/components/template/Pagina";
 import useCarrinho from "@/data/hooks/useCarrinho";
 
@@ -10,21 +11,25 @@ export default function PaginaCarrinho() {
     const { itens, adicionar, remover } = useCarrinho()
 
     return (
-        <Pagina>
+        <Pagina className="flex flex-col gap-10">
             {itens.length === 0 ? (
                 <CarrinhoVazio />
             ) : (
-                <div className="flex flex-col gap-5">
-                    {itens.map(item =>
-                        <AreaItemCarrinho
-                            key={item.produto.id}
-                            item={item}
-                            adicionar={(item) => adicionar(item.produto)}
-                            remover={(item) => remover(item.produto)}
-                        />
-                    )}
-                </div>
+                <>
+                    <div className="flex flex-col gap-5">
+                        {itens.map(item =>
+                            <AreaItemCarrinho
+                                key={item.produto.id}
+                                item={item}
+                                adicionar={(item) => adicionar(item.produto)}
+                                remover={(item) => remover(item.produto)}
+                            />
+                        )}
+                    </div>
+                    <TotalCarrinho itens={itens} />
+                </>
             )}
+
         </Pagina>
     )
 }
